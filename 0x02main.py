7f7205eaf6fddfd4231e4b37395e9a7ef77c8984
wilzdude04-star <wilzdude04@gmail.com>
2026-04-28 15:22:36 +0100

    Replace hardcoded user info with input prompts

--- 0x02main.py
+++ 0x02main.py
@@ -1,14 +1,4 @@
-name = "Alice"
-age = 25
-hobby = "painting"
-
-print(f"Hi, my name is {name}, I am {age} years old, and I love {hobby}!")
-
-
-
-
-name = "Bob"
-age = 30
-hobby = "coding"
-
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
 print(f"Hi, my name is {name}, I am {age} years old, and I love {hobby}!")
