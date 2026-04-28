6aab06c87ced7774834b4950a179ab2ba1b9d75f
wilzdude04-star <wilzdude04@gmail.com>
2026-04-28 15:21:32 +0100

    Implement personalized greeting function
    Added a new function to greet users by name.

--- 0x01main.py
+++ 0x01main.py
@@ -1,3 +1,6 @@
-def greet():
-    print("Hello, welcome to phythn!")
-greet()
+# This function takes a name and prints a personalized welcome message
+def personal_greet(name):
+ print(f"Hello, {name}! Welcome to Python!")
+
+personal_greet("Alice")
+personal_greet("Charlie")
