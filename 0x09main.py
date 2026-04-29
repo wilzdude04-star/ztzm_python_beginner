1e708e51a6b5b4812c0c6f5834a066a8ae04b383
wilzdude04-star <wilzdude04@gmail.com>
2026-04-29 14:04:24 +0100

    Add password verification loop in main.py

---
+++ 0x09main.py
@@ -0,0 +1,5 @@
+correct_password = "python123"
+user_input = input("Enter the password: ")
+while user_input != correct_password:
+ print("Incorrect password. Try again.")
+user_input = input("Enter the password: ")
